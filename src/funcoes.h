dea18f7945cd1f16eb4d6fe2743077c92a573808
rafaegont1 <109547317+rafaegont1@users.noreply.github.com>
2023-03-21 16:49:50 -0300

    Add files via upload

---
+++ src/funcoes.h
@@ -0,0 +1,12 @@
+#ifndef FUNCOES_H
+#define FUNCOES_H
+
+#include <stdio.h>
+
+typedef struct movement Mov;
+
+unsigned short greedyAlg(unsigned short mat_sz, unsigned short *mat);
+void move(unsigned short *i, unsigned short *j, char dir);
+void printMat(unsigned short mat_sz, unsigned short *mat);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,1 +1,35 @@
+#include "funcoes.h"
+#include <stdlib.h>
 
+int main() {
+	unsigned short *mat, mat_sz, n = 1, global_sum = 0, local_sum = 0;
+	FILE *fp = fopen("input.data", "r");
+
+	if(fp == NULL) {
+		puts("Erro ao abrir o arquivo");
+		exit(EXIT_FAILURE);
+	}
+
+	fscanf(fp, "%hu %*s\n", &mat_sz);
+
+	mat = (unsigned short *)malloc(mat_sz * mat_sz * sizeof(unsigned short));
+
+	while(!feof(fp)) {
+		for(int i = 0; i < mat_sz; i++) {
+			for(int j = 0; j < mat_sz; j++) {
+				fscanf(fp, "%hu ", &mat[mat_sz * i + j]);
+			}
+		}
+		printf("------------------Matriz número %hu------------------\n\n", n++);
+		printMat(mat_sz, mat);
+		global_sum += local_sum = greedyAlg(mat_sz, mat);
+		printf("Resultado local: %hu\n\n", local_sum);
+	}
+
+	printf("Resultado global: %hu\n", global_sum);
+
+	free(mat);
+	fclose(fp);
+
+	return 0;
+}
